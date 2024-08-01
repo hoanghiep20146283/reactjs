e3f43a2b3c55f773fe63408c3a9dd932187595f7
Hiệp Hoàng <hoanghiep20179706@gmail.com>
2024-08-01 07:42:46 +0700

    NextJS: Add loading and error page

--- src/app/courses/page.tsx
+++ src/app/courses/page.tsx
@@ -1,16 +1,38 @@
 import styles from './styles.module.css'
 
+interface CourseResponse {
+    successful: boolean,
+    result: Course,
+}
+
+type Course = {
+    id: string;
+    title: string;
+    description: string;
+    creationDate: string;
+    duration: number;
+    authors: string[];
+};
+
+function delay(ms: number): Promise<void> {
+    return new Promise(resolve => setTimeout(resolve, ms));
+  }
+
 const fetchData = async () => {
-    const courseResponse = await fetch(`http://localhost:4000/courses/${params.courseId}`, {
+    const courseResponse = await fetch(`http://localhost:4000/courses/66cc289e-6de9-49b2-9ca7-8b4f409d6467`, {
         method: 'GET',
         headers: {
             'Content-Type': 'application/json',
         },
     });
+    await delay(3000);
     const courseDetailResponse: CourseResponse = await courseResponse.json();
+    return courseDetailResponse.result;
+    // throw new Error();
 }
 
-export default function Courses({ params }: { params: { name: string } }) {
+export default async function Courses({ params }: { params: { name: string } }) {
+    const courseDetail = await fetchData();
     return (
         <div className={styles.CourseDetail} data-testid='CourseDetail'>
             <p className={styles.MainTitle}>{courseDetail?.title}</p>
@@ -28,7 +50,7 @@ export default function Courses({ params }: { params: { name: string } }) {
                     </div>
                     <div className={styles.InfoItem}>
                         <p className={styles.InfoName}>Duration:</p>
-                        <p className={styles.DurationInfo}>{formatDuration(courseDetail?.duration)}</p>
+                        <p className={styles.DurationInfo}>{courseDetail?.duration}</p>
                     </div><div className={styles.InfoItem}>
                         <p className={styles.InfoName}>Created:</p>
                         <p className={styles.InfoValue}>{courseDetail?.creationDate}</p>
@@ -37,9 +59,6 @@ export default function Courses({ params }: { params: { name: string } }) {
                         <p className={styles.InfoValue}>{courseDetail?.authors}</p>
                     </div>
                 </div>
-            </div>
-            <div className={styles.ButtonWrapper}>
-                <Link to='/courses'>Back</Link>
             </div>
         </div>
     );
---
+++ src/components/CourseDetail/CourseDetail.tsx
@@ -0,0 +1,78 @@
+import React, { FC, useEffect, useState } from 'react';
+import styles from './CourseDetail.module.css';
+
+interface CourseResponse {
+  successful: boolean,
+  result: Course,
+}
+
+type Course = {
+  id: string;
+  title: string;
+  description: string;
+  creationDate: string;
+  duration: number;
+  authors: string[];
+};
+
+const formatDuration = (durationInMinutes: number | undefined): string => {
+  if (durationInMinutes === undefined) {
+    return "Unknown"
+  } else if (durationInMinutes === 60) {
+    return '1 hour';
+  } else if (durationInMinutes > 60) {
+    const hours = Math.floor(durationInMinutes / 60);
+    return `${hours} hours`;
+  } else {
+    return `${durationInMinutes} minutes`;
+  }
+};
+
+const CourseDetail: FC = () => {
+  const params = useParams();
+  const [courseDetail, setCourseDetail] = useState<Course>();
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const courseResponse = await fetch(`http://localhost:4000/courses/${params.courseId}`, {
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      });
+      const courseDetailResponse: CourseResponse = await courseResponse.json();
+      setCourseDetail(courseDetailResponse.result);
+    }
+    fetchData();
+  }, []);
+  return (
+    <div className={styles.CourseDetail} data-testid='CourseDetail'>
+      <p className={styles.MainTitle}>{courseDetail?.title}</p>
+      <div className={styles.CourseWrapper}>
+        <div className={styles.Description}>
+          <p className={styles.Title}>Description:</p>
+          <p>{courseDetail?.description}</p>
+        </div>
+        <div className={styles.Divider}></div>
+        <div className={styles.Info}>
+          <div className={styles.Space}></div>
+          <div className={styles.InfoItem}>
+            <p className={styles.InfoName}>ID:</p>
+            <p className={styles.InfoValue}>{courseDetail?.id}</p>
+          </div>
+          <div className={styles.InfoItem}>
+            <p className={styles.InfoName}>Duration:</p>
+            <p className={styles.DurationInfo}>{formatDuration(courseDetail?.duration)}</p>
+          </div><div className={styles.InfoItem}>
+            <p className={styles.InfoName}>Created:</p>
+            <p className={styles.InfoValue}>{courseDetail?.creationDate}</p>
+          </div><div className={styles.InfoItem}>
+            <p className={styles.InfoName}>Authors: </p>
+            <p className={styles.InfoValue}>{courseDetail?.authors}</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+export default CourseDetail;
